dcf7ec787b520fbaa7286443ea11feb72ee922f2
Vipin Dhama <vipindhama201@yahoo.co.in>
2020-04-24 11:21:17 +0000

    Subtraction Logic Added

--- calculator.py
+++ calculator.py
@@ -1,6 +1,11 @@
 # Program make a simple calculator
 
 
+# This function subtracts two numbers 
+def subtract(x, y):
+   return x - y
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -12,3 +17,11 @@ choice = input("Enter choice(1/2/3/4): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+
+
+
+
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
