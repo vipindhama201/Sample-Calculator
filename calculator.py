65afaf55b92634ee3c971ed44e75c8e82e9d539c
Vipin Dhama <vipindhama201@yahoo.co.in>
2020-04-24 11:16:00 +0000

    Addition logic added

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
 # Program make a simple calculator
 
+# This function adds two numbers 
+def add(x, y):
+   return x + y
 
 print("Select operation.")
 print("1.Add")
@@ -12,3 +15,9 @@ choice = input("Enter choice(1/2/3/4): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+else:
+   print("Invalid input")
